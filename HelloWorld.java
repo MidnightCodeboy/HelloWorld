91e6d2089e108cd8748523b3a938b92d44f205e6
MidnightCodeboy <mellysguy@hotmail.co.uk>
2014-11-09 21:51:12 -0400

    More modifications

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld{
 	public static void main(Sting[] args){
 		System.out.print("Hello World!\nHello some more");
+		for (int 1 = 0; i < 6; 1++){
+			System.out.print(String.format("%d", i*i));
+		}
 	}
 }
